23c6c14c7269f059f90c2c02ced9290121ae928c
Sam Jackson <samuelsjackson7@gmail.com>
2019-06-28 12:50:59 -0400

    podcasts related to the topic entered show up on submit

--- assets/javascript/mainpage.js
+++ assets/javascript/mainpage.js
@@ -8,61 +8,80 @@ $( document ).ready(function() {
 
     function displayPods() {
 
+        event.preventDefault();
+
         var key = "2ce331e1a7d34875879ce8fc37eded8f";
 
-        var query = "soccer";
+        // retrieve topic from input box
+        var topic = $("#topic").val();;
+        console.log("topic: " + topic)
 
-        var minLength = 10;
+        var minLength = 40;
 
-        var maxLength = 300;
+        var maxLength = 60;
 
-        var queryURL = "https://listen-api.listennotes.com/api/v2/search?q=" + query + "&sort_by_date=0&type=episode&offset=0&len_min=" + minLength + "&len_max=" + maxLength + "&genre_ids=68%2C82&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1";
-
-        // query = $("#genre").val();
+        var queryURL = "https://listen-api.listennotes.com/api/v2/search?q=" + topic + "&sort_by_date=0&type=episode&offset=0&len_min=" + minLength + "&len_max=" + maxLength + "&genre_ids=68%2C82&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1";
 
         $.ajax({
             url: queryURL,
             method: 'GET',
             headers: {
-                "X-ListenAPI-Key": "2ce331e1a7d34875879ce8fc37eded8f"
+                "X-ListenAPI-Key": key
             }
             }).then(response => {
                 
                 console.log(response);
 
+                $("#podcast-container").empty();
+
                 var podArr = response.results;
 
-                for (i=0; i<5; i++) {
+                for (i=0; i < podArr.length / 2; i++) {
+
+                    //creating a div to hold the podcast info
+                    var div = $("<div>");
+                    div.attr("id", "podDiv");
+
+                    // retrieving the thumbnail data
+                    var podThumbnail = podArr[i].thumbnail;
                 
-                    var thumb = $("<img>").attr("src", podArr[i].thumbnail);
-                    $("#podcast-container").append(thumb);
-                    $("#podcast-container").append("<br>");
+                    // creating an element to hold the thumbnail
+                    var thumb = $("<img>").attr("src", podThumbnail);
 
-                    var title = $("<p>").text(podArr[i].title_original);
-                    $("#podcast-container").append(title);
+                    // Displaying the thumbnail
+                    div.append(thumb);
 
+                    // Retrieving the podcast title
+                    var podTitle = podArr[i].title_original
 
+                    // Creating an element to hold the title
+                    var title = $("<p>").text(podTitle);
+
+                    // Displaying the title
+                    div.append(title);
+
+                    // Creating an element to hold the audio
                     var audio = $("<audio controls>");
                     var audioSrc = $("<source>").attr("src", podArr[i].audio);
                     audio.append(audioSrc);
+                    
+                    // Displaying the audio
+                    div.append(audio);
 
-                    $("#podcast-container").append(audio);
-                    $("#podcast-container").append("<br>");
-                    $("#podcast-container").append("<br>");
+                    // putting the entire div below the previous
+                    $("#podcast-container").append(div);
+                    console.log(i);
                 }
+
+                console.log("WE got 5");
             });
-
-
-            var map;
-
-            function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: -34.397, lng: 150.644},
-                    zoom: 8
-            });
-            }
-
     }
+
+
+    $(document).on("click", "#submit-button", displayPods);
+
+
+
     
 })
 
