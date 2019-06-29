ff6f6402112ea11cf78853199d1ff2261daa6617
Marine809 <rthorpesr@verizon.net>
2019-06-29 18:53:48 -0400

    commented out console log statements

--- assets/javascript/mainpage.js
+++ assets/javascript/mainpage.js
@@ -1,44 +1,44 @@
-$( document ).ready(function() {
+$( document ).ready(function()
+     {
 
-    $(".dropdown-trigger").dropdown();
+         $(".dropdown-trigger").dropdown();
     
-    var inLocation = "500 E. Libert Road, Rockville, MD";
-var inDestination = "thorpesr@"
-console.log("starting Location: " +inLocation);
-console.log("Your Destination: " +inDestination);''
-
-
-
-  inLocation = "Smith";
-  console.log("inLocation should be spaces");
-
-  if (inLocation.length == 0)
-  {
-    console.log("THere");
-      console.log("Please enter a starting Location:" +inLocation);
-    }
-    else {
-        console.log("fore");
-        console.log("Here is the starting Location:" +inLocation);
-        console.log("Here is the starting Destination:" +inDestination);
-        
-
-    }
-
-// Starting Location can't be blank
-$('#contact_name').on('input', function() {
-	var input=$(this);
-	var is_name=input.val();
-	if(is_name){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
-
-// Destination can't be blank
-$('#contact_name').on('input', function() {
-	var input=$(this);
-	var is_name=input.val();
-	if(is_name){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
-         }
-})
+    
+         $("#submit-button").on("click", function(event) 
+              {
+                   event.preventDefault();
+                      
+                   var inlocation = $('#location').val();
+                   //console.log("Starting Location:" +inlocation);  
+                   var indestination = $('#destination').val();
+                   //console.log("Your Destination:" +indestination);
+                   
+                   if ((inlocation == 0) && (indestination == 0)) 
+                        {
+                            // Starting Location and Destination are blank
+                           // console.log("MISSING THE LOCATION");
+                           // console.log("Both missing - Starting Location:" +inlocation);
+                           // console.log("Both missing - Your Destination:" +indestination);
+                            $('audio#pop1')[0].play()
+                            //$('#location').val('Location cannot be blank').css('color', 'red');
+                           // $('#destination').val('Destination cannot be blank').css('color', 'red');
+                        }
+                    else
+                        if ((inlocation == 0) && (indestination !== 0))
+                             {
+                                  // Starting Location is blank and Destination is not
+                                 // console.log("Just Location missing - Starting Location:" +inlocation);
+                                  $('audio#pop2')[0].play()
+                             //     $('#location').val('Location cannot be blank').css('color', 'red');
+                             }
+                        else
+                           if ((indestination == 0) && (inlocation !== 0)) 
+                                  {
+                                       //  Destination is blank and Starting Location is not
+                                      // console.log("MISSING THE DESTINATION");
+                                       //console.log("Just Dest missing - Your Destination:" +indestination);    
+                                       $('audio#pop3')[0].play()
+                                      // $('#destination').val('Destination cannot be blank').css('color', 'red');
+                                  }  
+              })        
+    })
