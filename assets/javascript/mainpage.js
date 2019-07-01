a5b4477609e36e65ec55a1422584e87dce31274f
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-01 13:50:14 -0400

    Rearranged code so that all audio calls work. Changed form so that it will not submit and pull unless all fields are filled out.

--- assets/javascript/mainpage.js
+++ assets/javascript/mainpage.js
@@ -87,43 +87,90 @@ $( document ).ready(function() {
 
     
     $("#submit-button").on("click", function(){
-        $("#submission-box").hide();
-        $("#podcast-card").show();
 
+     if ($.trim($("#location").val()) === "" || $.trim($("#destination").val()) === "" || $.trim($("#topic").val()) === "") {
         event.preventDefault();
 
-    var inlocation = $('#location').val();
-    //console.log("Starting Location:" +inlocation);
-    var indestination = $('#destination').val();
-    //console.log("Your Destination:" +indestination);
-
-    if ((inlocation == 0) && (indestination == 0))
-         {
-             // Starting Location and Destination are blank
-            // console.log("MISSING THE LOCATION");
-            // console.log("Both missing - Starting Location:" +inlocation);
-            // console.log("Both missing - Your Destination:" +indestination);
-             $('audio#pop1')[0].play()
-             //$('#location').val('Location cannot be blank').css('color', 'red');
-            // $('#destination').val('Destination cannot be blank').css('color', 'red');
-         }
-     else
-         if ((inlocation == 0) && (indestination !== 0))
-              {
-                   // Starting Location is blank and Destination is not
-                  // console.log("Just Location missing - Starting Location:" +inlocation);
-                   $('audio#pop2')[0].play()
-              //     $('#location').val('Location cannot be blank').css('color', 'red');
-              }
+        var inlocation = $('#location').val();
+        //console.log("Starting Location:" +inlocation);
+        var indestination = $('#destination').val();
+        //console.log("Your Destination:" +indestination);
+    
+        if ((inlocation == 0) && (indestination == 0))
+             {
+                 // Starting Location and Destination are blank
+                // console.log("MISSING THE LOCATION");
+                // console.log("Both missing - Starting Location:" +inlocation);
+                // console.log("Both missing - Your Destination:" +indestination);
+                 $('audio#pop1')[0].play()
+                 //$('#location').val('Location cannot be blank').css('color', 'red');
+                // $('#destination').val('Destination cannot be blank').css('color', 'red');
+             }
          else
-            if ((indestination == 0) && (inlocation !== 0))
-                   {
-                        //  Destination is blank and Starting Location is not
-                       // console.log("MISSING THE DESTINATION");
-                        //console.log("Just Dest missing - Your Destination:" +indestination);
-                        $('audio#pop3')[0].play()
-                       // $('#destination').val('Destination cannot be blank').css('color', 'red');
-                   }
+             if ((inlocation == 0) && (indestination !== 0))
+                  {
+                       // Starting Location is blank and Destination is not
+                      // console.log("Just Location missing - Starting Location:" +inlocation);
+                       $('audio#pop2')[0].play()
+                  //     $('#location').val('Location cannot be blank').css('color', 'red');
+                  }
+             else
+                if ((indestination == 0) && (inlocation !== 0))
+                       {
+                            //  Destination is blank and Starting Location is not
+                           // console.log("MISSING THE DESTINATION");
+                            //console.log("Just Dest missing - Your Destination:" +indestination);
+                            $('audio#pop3')[0].play()
+                           // $('#destination').val('Destination cannot be blank').css('color', 'red');
+                        } 
+            return false;
+            }
+
+
+
+
+
+           else {
+               
+            $("#submission-box").hide();
+            $("#podcast-card").show();
+             event.preventDefault();
+
+           }
+
+    // var inlocation = $('#location').val();
+    // //console.log("Starting Location:" +inlocation);
+    // var indestination = $('#destination').val();
+    // //console.log("Your Destination:" +indestination);
+
+    // if ((inlocation == 0) && (indestination == 0))
+    //      {
+    //          // Starting Location and Destination are blank
+    //         // console.log("MISSING THE LOCATION");
+    //         // console.log("Both missing - Starting Location:" +inlocation);
+    //         // console.log("Both missing - Your Destination:" +indestination);
+    //          $('audio#pop1')[0].play()
+    //          //$('#location').val('Location cannot be blank').css('color', 'red');
+    //         // $('#destination').val('Destination cannot be blank').css('color', 'red');
+    //      }
+    //  else
+    //      if ((inlocation == 0) && (indestination !== 0))
+    //           {
+    //                // Starting Location is blank and Destination is not
+    //               // console.log("Just Location missing - Starting Location:" +inlocation);
+    //                $('audio#pop2')[0].play()
+    //           //     $('#location').val('Location cannot be blank').css('color', 'red');
+    //           }
+    //      else
+    //         if ((indestination == 0) && (inlocation !== 0))
+    //                {
+    //                     //  Destination is blank and Starting Location is not
+    //                    // console.log("MISSING THE DESTINATION");
+    //                     //console.log("Just Dest missing - Your Destination:" +indestination);
+    //                     $('audio#pop3')[0].play()
+    //                    // $('#destination').val('Destination cannot be blank').css('color', 'red');
+    //                 } 
+                //    }
 })
 
 
